40fa0c7b861cb783619c50c5bbf7493456b60fb0
NathanGrenier <nathangrenier01@gmail.com>
2024-01-13 19:09:04 -0500

    removed speed insight
    it was erroring out and isn't worth fixing for now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,7 @@
 <script>
-  import "../app.css";
+  import "../app.postcss";
   import { dev } from "$app/environment";
   import { inject } from "@vercel/analytics";
-  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
-
-  injectSpeedInsights();
 
   inject({ mode: dev ? "development" : "production" });
 </script>
