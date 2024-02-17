b650cbd4ddb4bd9a00fb66a4ef69df8f1f56ef98
Brian Tkatch <brian@briantkatch.com>
2024-02-17 15:11:39 -0500

    Add session generation, verification and refresh
    src/hooks.server.ts - checks session token on each payload. If valid, saves the critical user properties to the event locals. Critically, routes that are supposed to be open need to be whitelisted in this file.
    
    src/app.d.ts: This contains the typescript definitions for the user event local.
    
    src/lib/server/session.ts - this code generates and verifies the JWTs. There is some cleanup to be done here with TypeScript optionals.
    
    Package file: had to add jsonwebtoken, and types for jsonwebtoken/bcrypt

---
+++ src/hooks.server.ts
@@ -0,0 +1,26 @@
+import { redirect } from "@sveltejs/kit";
+import { validateAndRefreshSession } from "$lib/server/session";
+
+const openRoutes = ["/", "/auth/login", "/auth/logout"];
+/** @type {import('@sveltejs/kit').Handle} */
+export const handle = async ({ event, resolve }) => {
+  const session = String(event.cookies.get("SvelteState-Session") || "");
+  if (!session && !openRoutes.includes(event.url.pathname)) {
+    throw redirect(307, `/auth/login?destination=${event.url.pathname}`);
+  }
+
+  const result = validateAndRefreshSession(session);
+  if (!result.success || event.url.pathname === "/auth/logout") {
+    // Delete cookie if session invalid or route is logout
+    await event.cookies.delete("SvelteState-Session", { path: "/" });
+    if (!openRoutes.includes(event.url.pathname))
+      throw redirect(307, `/auth/login?destination=${event.url.pathname}`);
+  } else {
+    event.locals.user = result.user;
+    event.cookies.set("SvelteState-Session", String(result.newSession), {
+      path: "/",
+    });
+  }
+
+  return resolve(event);
+};
---
+++ src/lib/server/session.ts
@@ -0,0 +1,69 @@
+import { UserRole } from "@prisma/client";
+import jwt from "jsonwebtoken";
+
+const key = Buffer.from(process.env.SESSION_SIGNING_KEY || "", "hex");
+const aud = process.env.EXEC_ENV;
+
+export const createSession = (claims: {
+  sub: string;
+  name: string;
+  email: string;
+  role: UserRole;
+  iat?: number; // Preserved if passed, set to now if not.
+}): string => {
+  const now = Math.floor(new Date().getTime() / 1000);
+  return jwt.sign(
+    {
+      iss: "SvelteState",
+      aud: aud,
+      iat: claims.iat || now,
+      nbf: now,
+      exp: now + 14400,
+      sub: claims.sub,
+      name: claims.name,
+      email: claims.name,
+      role: claims.role,
+    },
+    key,
+    { algorithm: "HS256", noTimestamp: true },
+  );
+};
+
+export const validateAndRefreshSession = (
+  token: string,
+): {
+  success: boolean;
+  user?: {
+    id: number;
+    name: string;
+    email: string;
+    role: UserRole;
+  };
+  newSession?: string;
+} => {
+  try {
+    const decoded = jwt.verify(token, key, {
+      algorithms: ["HS256"],
+      audience: aud,
+    });
+    // Need to force-deal with all of these optionals
+    // prettier-ignore
+    // @ts-expect-error JWT has no rich type data
+    const newSession = createSession({ sub: decoded.sub, name: decoded.name, email: decoded.email, role: decoded.role, iat: decoded.iat, });
+    return {
+      success: true,
+      user: {
+        id: Number(decoded.sub),
+        /// @ts-expect-error JWT has no rich type data
+        name: decoded.name,
+        // @ts-expect-error JWT has no rich type data
+        email: decoded.email,
+        // @ts-expect-error JWT has no rich type data
+        role: decoded.role,
+      },
+      newSession: newSession,
+    };
+  } catch (error) {
+    return { success: false };
+  }
+};
