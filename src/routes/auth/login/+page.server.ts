94cd29238743b5bd99adee69c0c8154f29e052bd
Brian Tkatch <brian@briantkatch.com>
2024-02-17 15:12:20 -0500

    Add login route and form
    Some work to be done here to ensure that client and server validation works before passing off to front end team.

---
+++ src/routes/auth/login/+page.server.ts
@@ -0,0 +1,50 @@
+import bcrypt from "bcrypt";
+import { prisma } from "../../../lib/db/client";
+import { fail, redirect } from "@sveltejs/kit";
+import { createSession } from "$lib/server/session";
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+  default: async (event) => {
+    const form = await event.request.formData();
+    const email = String(form.get("email") || "");
+    const passwordRaw = String(form.get("password") || "");
+    if (!email) {
+      return fail(400, { email, missing: true });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (
+      !user ||
+      user.disabled ||
+      !(await bcrypt.compare(passwordRaw, user.passwordHash))
+    ) {
+      return fail(400, { email, incorrect: true });
+    }
+
+    console.log("here");
+
+    event.cookies.set(
+      "SvelteState-Session",
+      createSession({
+        sub: String(user.id),
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      }),
+      { path: "/" },
+    );
+
+    // Prevent open redirect, only allow destinations starting with /
+    if ((event.url.searchParams.get("destination") || "").indexOf("/") == 0) {
+      return redirect(302, event.url.searchParams.get("destination") || "/");
+    } else {
+      return redirect(302, "/");
+    }
+  },
+};
---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,19 @@
+<div class="flex flex-col items-center justify-center gap-4">
+  <h1 class="text-4xl font-bold">Log In</h1>
+  <form method="POST">
+    <input
+      class="input"
+      title="Email address"
+      name="email"
+      type="text"
+      placeholder="you@somewhere.example"
+      required />
+    <input
+      class="input"
+      title="Password"
+      name="password"
+      type="password"
+      required />
+    <button class="variant-filled btn" type="submit">Log in</button>
+  </form>
+</div>
