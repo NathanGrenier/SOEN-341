871b74f637bdc2a452c9795c8002bcf51acabf26
NathanGrenier <nathangrenier01@gmail.com>
2024-02-17 21:46:12 -0500

    feat: changed form layout and styling. Added non-functional create account button

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,19 +1,36 @@
-<div class="flex flex-col items-center justify-center gap-4">
-  <h1 class="text-4xl font-bold">Log In</h1>
-  <form method="POST">
-    <input
-      class="input"
-      title="Email address"
-      name="email"
-      type="text"
-      placeholder="you@somewhere.example"
-      required />
-    <input
-      class="input"
-      title="Password"
-      name="password"
-      type="password"
-      required />
-    <button class="variant-filled btn" type="submit">Log in</button>
-  </form>
+<div class="flex justify-center">
+  <div class="card max-w-xl flex-grow">
+    <header class="text4xl card-header flex justify-center font-bold">
+      <h1 class="text-4xl">Log In</h1>
+    </header>
+    <section class="p-4">
+      <form method="POST" class="flex flex-col gap-3">
+        <label class="label">
+          <span>Email</span>
+          <input
+            class="input"
+            title="Email address"
+            name="email"
+            type="text"
+            placeholder="you@somewhere.example"
+            required />
+        </label>
+        <label class="label">
+          <span>Password</span>
+          <input
+            class="input"
+            title="Password"
+            name="password"
+            type="password"
+            required />
+        </label>
+        <button class="variant-filled-primary btn mt-4" type="submit"
+          >Log in</button>
+      </form>
+    </section>
+    <footer class="card-footer mt-2 flex items-center justify-around">
+      <p class="font-bold">Don't have an account?</p>
+      <button class="variant-filled-secondary btn">Create Account</button>
+    </footer>
+  </div>
 </div>
