1ef211c45216306fde486a06a2c46f432fa79bc2
NathanGrenier <nathangrenier01@gmail.com>
2024-03-11 21:48:55 -0400

    style: fixed formatting

--- src/routes/browse-vehicles/+page.svelte
+++ src/routes/browse-vehicles/+page.svelte
@@ -50,7 +50,7 @@
           <p>{car.description}</p>
           <button
             on:click={() => showPopup(index)}
-            class="cursor-pointer rounded-lg border-b-[4px] border-blue-600 bg-blue-500 px-6 py-2 text-white transition-all hover:-translate-y-[1px] hover:border-b-[6px] hover:brightness-110 active:translate-y-[2px] active:border-b-[2px] active:brightness-90">
+            class="border-blue-600 bg-blue-500 text-white cursor-pointer rounded-lg border-b-[4px] px-6 py-2 transition-all hover:-translate-y-[1px] hover:border-b-[6px] hover:brightness-110 active:translate-y-[2px] active:border-b-[2px] active:brightness-90">
             Button
           </button>
         </div>
