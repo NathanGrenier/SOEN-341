89b04bf0e9366776c82e9ec377128d7a1215dafc
NathanGrenier <nathangrenier01@gmail.com>
2024-03-12 00:13:47 -0400

    feat: highlight the selected route on mobile

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
+  import { page } from "$app/stores";
+  import CarIcon from "$lib/icons/CarIcon.svelte";
+  import HomeIcon from "$lib/icons/HomeIcon.svelte";
   import { getDrawerStore } from "@skeletonlabs/skeleton";
 
   const links = [
-    { name: "Home", href: "/" },
-    { name: "About", href: "/about" },
+    { name: "Home", href: "/", icon: HomeIcon },
+    { name: "Browse Vehicles", href: "/browse-vehicles", icon: CarIcon },
   ];
 
+  $: selected = $page.url.pathname;
   const drawerStore = getDrawerStore();
 
   function drawerClose() {
@@ -16,7 +20,13 @@
 <nav class="list-nav p-4">
   <ul>
     {#each links as link (link.name)}
-      <li><a href={link.href} on:click={drawerClose}>{link.name}</a></li>
+      <li>
+        <a
+          class={selected === link.href ? "variant-filled-primary" : ""}
+          href={link.href}
+          on:click={drawerClose}
+          ><svelte:component this={link.icon} /><span>{link.name}</span></a>
+      </li>
     {/each}
   </ul>
 </nav>
