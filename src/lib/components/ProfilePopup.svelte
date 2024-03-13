0c784873fbfb657fcb344a1e57d04b0129fa7962
NathanGrenier <nathangrenier01@gmail.com>
2024-03-13 07:06:44 -0400

    reverted popup changes

--- src/lib/components/ProfilePopup.svelte
+++ src/lib/components/ProfilePopup.svelte
@@ -12,46 +12,49 @@
   $: loggedIn = !!user;
 </script>
 
-<div class="variant-filled arrow" />
+<div
+  style="margin: 1px"
+  class="card variant-filled w-auto p-4 shadow-xl"
+  data-popup="profilePopup">
+  <div class="variant-filled arrow" />
 
-{#if loggedIn}
-  <div class=" variant-filled btn-group-vertical w-full">
-    <!-- TODO: Un-disable the button when the profile page is implemented -->
-    <button
-      style="justify-content: start;"
-      class=" bg-secondary-hover-token gap-2"
-      disabled={true}
-      ><UserIcon invertColor={true} /><span>Profile</span></button>
-    <a
-      href="/dashboard"
-      style="justify-content: start;"
-      class=" bg-secondary-hover-token gap-2"
-      ><GridIcon invertColor={true} /><span>Dashboard</span></a>
-    {#if user?.role === UserRole.ADMIN}
-      <a
-        href="/admin"
+  {#if loggedIn}
+    <div class=" variant-filled btn-group-vertical w-full">
+      <!-- TODO: Un-disable the button when the profile page is implemented -->
+      <button
         style="justify-content: start;"
         class=" bg-secondary-hover-token gap-2"
-        ><GridPlusIcon invertColor={true} /><span>Admin Dashboard</span></a>
-    {/if}
-    <!-- TODO: Un-disable the button when the settings page is implemented -->
-    <button
-      style="justify-content: start;"
-      class="bg-secondary-hover-token gap-2"
-      disabled={true}
-      ><UserSettingsIcon invertColor={true} /><span>Settings</span></button>
-  </div>
-  <div>
-    <a
-      class="variant-filled-error btn mt-2 w-full"
-      href="/auth/logout"
-      data-sveltekit-reload>Log Out</a>
-  </div>
-{:else}
-  <div>
-    <a class="variant-filled-primary btn mt-2 w-full" href="/auth/login"
-      >Log In</a>
-    <a class="variant-filled-secondary btn mt-2 w-full" href="/auth/register"
-      >Create Account</a>
-  </div>
-{/if}
+        disabled={true}
+        ><UserIcon invertColor={true} /><span>Profile</span></button>
+      <a
+        href="/dashboard"
+        style="justify-content: start;"
+        class=" bg-secondary-hover-token gap-2"
+        ><GridIcon invertColor={true} /><span>Dashboard</span></a>
+      {#if user?.role === UserRole.ADMIN}
+        <a
+          href="/admin"
+          style="justify-content: start;"
+          class=" bg-secondary-hover-token gap-2"
+          ><GridPlusIcon invertColor={true} /><span>Admin Dashboard</span></a>
+      {/if}
+      <!-- TODO: Un-disable the button when the settings page is implemented -->
+      <button
+        style="justify-content: start;"
+        class="bg-secondary-hover-token gap-2"
+        disabled={true}
+        ><UserSettingsIcon invertColor={true} /><span>Settings</span></button>
+    </div>
+    <div>
+      <a class="variant-filled-error btn mt-2 w-full" href="/auth/logout"
+        >Log Out</a>
+    </div>
+  {:else}
+    <div>
+      <a class="variant-filled-primary btn mt-2 w-full" href="/auth/login"
+        >Log In</a>
+      <a class="variant-filled-secondary btn mt-2 w-full" href="/auth/register"
+        >Create Account</a>
+    </div>
+  {/if}
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -38,15 +38,10 @@
   import ModalComponentTest from "$lib/components/modals/ModalComponentTest.svelte";
   import Navigation from "$lib/components/Navigation.svelte";
   import HamburgerMenuIcon from "$lib/icons/HamburgerMenuIcon.svelte";
-  // import ProfilePopup from "$lib/components/ProfilePopup.svelte";
+  import ProfilePopup from "$lib/components/ProfilePopup.svelte";
   import { page } from "$app/stores";
   import CarIcon from "$lib/icons/CarIcon.svelte";
   import HomeIcon from "$lib/icons/HomeIcon.svelte";
-  import UserIcon from "$lib/icons/UserIcon.svelte";
-  import GridIcon from "$lib/icons/GridIcon.svelte";
-  import GridPlusIcon from "$lib/icons/GridPlusIcon.svelte";
-  import { UserRole } from "@prisma/client";
-  import UserSettingsIcon from "$lib/icons/UserSettingsIcon.svelte";
 
   storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 
@@ -132,62 +127,8 @@
             width="w-12"
             border="border-4 border-surface-300-600-token hover:!border-primary-500"
             cursor="cursor-pointer" />
-        </div>
-        <div
-          style="margin: 1px"
-          class="card variant-filled w-auto p-4 shadow-xl"
-          data-popup="profilePopup">
-          <div class="variant-filled arrow" />
-
-          {#if data.user}
-            <div class=" variant-filled btn-group-vertical w-full">
-              <!-- TODO: Un-disable the button when the profile page is implemented -->
-              <button
-                style="justify-content: start;"
-                class=" bg-secondary-hover-token gap-2"
-                disabled={true}
-                ><UserIcon invertColor={true} /><span>Profile</span></button>
-              <a
-                href="/dashboard"
-                style="justify-content: start;"
-                class=" bg-secondary-hover-token gap-2"
-                ><GridIcon invertColor={true} /><span>Dashboard</span></a>
-              {#if data.user?.role === UserRole.ADMIN}
-                <a
-                  href="/admin"
-                  style="justify-content: start;"
-                  class=" bg-secondary-hover-token gap-2"
-                  ><GridPlusIcon invertColor={true} /><span
-                    >Admin Dashboard</span
-                  ></a>
-              {/if}
-              <!-- TODO: Un-disable the button when the settings page is implemented -->
-              <button
-                style="justify-content: start;"
-                class="bg-secondary-hover-token gap-2"
-                disabled={true}
-                ><UserSettingsIcon invertColor={true} /><span>Settings</span
-                ></button>
-            </div>
-            <div>
-              <a
-                class="variant-filled-error btn mt-2 w-full"
-                href="/auth/logout"
-                data-sveltekit-reload>Log Out</a>
-            </div>
-          {:else}
-            <div>
-              <a
-                class="variant-filled-primary btn mt-2 w-full"
-                href="/auth/login">Log In</a>
-              <a
-                class="variant-filled-secondary btn mt-2 w-full"
-                href="/auth/register">Create Account</a>
-            </div>
-          {/if}
-          <!-- <ProfilePopup user={data.user} /> -->
-        </div>
-      </svelte:fragment>
+          <ProfilePopup user={data.user} />
+        </div></svelte:fragment>
     </AppBar>
   </svelte:fragment>
   <div class="container mx-auto p-10">
