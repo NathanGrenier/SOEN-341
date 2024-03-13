e9a6466b6685a4184528e845f6a26f87007d7f05
NathanGrenier <nathangrenier01@gmail.com>
2024-03-13 10:45:53 -0400

    build: re-added purgeCss

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
 // See https://www.skeleton.dev/docs/purgecss for details on how to configure PurgeCSS (specificly for safelisting)
-// import { purgeCss } from "vite-plugin-tailwind-purgecss";
+import { purgeCss } from "vite-plugin-tailwind-purgecss";
 
 export default defineConfig({
   plugins: [
     sveltekit(),
-    // purgeCss({
-    //   safelist: {
-    //     greedy: [/^hljs-/],
-    //   },
-    // }),
+    purgeCss({
+      safelist: {
+        greedy: [/^hljs-/],
+      },
+    }),
   ],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
