a3611530a0d156f82584ff4351273b76a7661ecd
NathanGrenier <nathangrenier01@gmail.com>
2024-03-16 23:49:24 -0400

    wip: reservation view

--- src/routes/dashboard/reservation-[reservationId]/+page.svelte
+++ src/routes/dashboard/reservation-[reservationId]/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import type { PageData } from "./$types";
+  import SeparatedText from "./SeparatedText.svelte";
+  import EditIcon from "$lib/icons/EditIcon.svelte";
 
   export let data: PageData;
 
@@ -20,56 +22,49 @@
       </h2>
     </header>
     <section class="p-4">
-      <div class="grid">
-        <div class="grid-item">
-          <!-- TODO: Change the url to the actual path -->
-
+      <div class="grid grid-cols-1 gap-x-10 gap-y-4 lg:grid-cols-2">
+        <div class="grid-item grid- row-span-2 w-full">
           <a class="card card-hover" href={`/cars/${car.id}`}>
-            <!-- TODO: Change the url to `{car.photoURL}`-->
             <img
-              src="../../../tempCar.png"
-              class="h-auto max-h-64 w-full rounded-md"
+              src={car.photoUrl}
+              class="h-full w-full rounded-lg"
               alt={`${car.make} ${car.model} ${car.year}`} />
           </a>
         </div>
         <div class="grid-item">
-          <h3 class="text-lg font-bold">Reservation Details</h3>
-          <input
-            class="input"
-            title="Date and time of planned departure"
-            bind:value={reservation.plannedDepartureAt}
-            type="datetime-local" />
-          <p>{reservation.plannedDepartureAt}</p>
-          <p>{reservation.plannedReturnAt}</p>
-          <p>${(reservation.quotedPrice / 100).toFixed(2)}</p>
+          <div class="flex items-center justify-between">
+            <h2 class=" text-2xl font-bold">Reservation Details</h2>
+            <button class="variant-filled-primary btn w-fit self-end"
+              ><EditIcon invertColor={true} constColor={true} /><span
+                >Modify</span
+              ></button>
+          </div>
+          <div class="mt-2 flex flex-col gap-1">
+            <input
+              class="input"
+              title="Date and time of planned departure"
+              bind:value={reservation.plannedDepartureAt}
+              type="datetime-local" />
+            <p>{reservation.plannedDepartureAt}</p>
+            <p>{reservation.plannedReturnAt}</p>
+            <p>${(reservation.quotedPrice / 100).toFixed(2)}</p>
+          </div>
         </div>
 
         <div class="grid-item">
-          <h3 class="text-lg font-bold">Branch Details</h3>
-          <p>{branch.name}</p>
-          <p>{branch.streetAddress}</p>
-          <p>{branch.city}</p>
-          <p>{branch.region}</p>
-          <p>{branch.postalCode}</p>
-        </div>
-        <div class="grid-item">
-          <h3 class="text-lg font-bold">Branch Location</h3>
+          <h2 class="text-2xl font-bold">Branch Details</h2>
+          <div class="mt-2 flex flex-col gap-1">
+            <SeparatedText title="Name" content={branch.name} />
+            <SeparatedText
+              title="Street Address"
+              content={branch.streetAddress} />
+            <SeparatedText title="City" content={branch.city} />
+            <SeparatedText title="Region" content={branch.region} />
+            <SeparatedText title="Postal Code" content={branch.postalCode} />
+          </div>
         </div>
       </div>
     </section>
-    <footer class="card-footer">(footer)</footer>
+    <!-- <footer class="card-footer">(footer)</footer> -->
   </div>
 </div>
-
-<style>
-  .grid {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr); /* Define 2 columns */
-    gap: 10px; /* Define the gap between grid items */
-  }
-
-  .grid-item {
-    width: 100%; /* Make the grid item take up the full width of its grid cell */
-    /* aspect-ratio: 1 / 1; Make the grid item a square */
-  }
-</style>
---
+++ src/routes/dashboard/reservation-[reservationId]/SeparatedText.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let title: string;
+  export let content: string;
+</script>
+
+<div class="flex items-baseline justify-between gap-2">
+  <h3 class="font-bold">{title}:</h3>
+  <div
+    class="flex-grow border-b-4 border-dotted border-black dark:border-white">
+  </div>
+  <p>{content}</p>
+</div>
