b1e3d89106235b2ad7ab9bc18cc792d4c5a78332
Brian Tkatch <brian@briantkatch.com>
2024-03-27 15:45:16 -0400

    Remove unneccesary list call in +layout.server.ts

--- src/routes/admin/+layout.server.ts
+++ src/routes/admin/+layout.server.ts
@@ -1,7 +1,7 @@
 import { error, redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "../$types";
 import { UserRole } from "@prisma/client";
-import { list } from "$lib/server/blob";
+//import { list } from "$lib/server/blob";
 
 export const load: LayoutServerLoad = async ({ locals }) => {
   if (!locals.user) {
@@ -10,6 +10,7 @@ export const load: LayoutServerLoad = async ({ locals }) => {
   if (locals.user.role !== UserRole.ADMIN) {
     return error(401, `You don't have permission to access this page.`);
   }
-  const { blobs } = await list();
-  return { blobs };
+  // This works, but it isn't used and will run up the S3 bill
+  // const { blobs } = await list();
+  // return { blobs };
 };
