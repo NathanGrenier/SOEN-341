51ac6683fe490fb0e4932a7a1a8f5681ebb295ff
NathanGrenier <nathangrenier01@gmail.com>
2024-04-10 23:34:32 -0400

    fix: removed padding and margin from anchor tag (reduce clickable area)

--- src/lib/components/HomePageCard.svelte
+++ src/lib/components/HomePageCard.svelte
@@ -8,49 +8,51 @@
   id={`section${card.section}`}
   data-section={card.section}
   class="section-card fade-transition {isVisible ? 'visible' : ''}">
-  <a
-    href={card.href}
-    class="relative my-4 grid grid-cols-2 items-center justify-center gap-4 space-y-6 p-40">
-    {#if card.section % 2 === 0}
-      <div class="col-span-1">
-        <img
-          src={card.previewImage}
-          alt=""
-          class="img card-hover flex justify-center rounded-md align-middle" />
-      </div>
-      <div class="card card-hover col-span-1">
-        <img class="rounded-t-lg" src={card.image} alt={card.title} />
-        <header class="card-header">
-          <p class="h2">{card.title}</p>
-        </header>
-        <section class="p-4">
-          {card.description}
-        </section>
-        <footer class="card-footer flex justify-center">
-          <a href={card.href} class="btn bg-primary-500">{card.buttonText}</a>
-        </footer>
-      </div>
-    {:else}
-      <div class="card card-hover col-span-1">
-        <img class="rounded-t-lg" src={card.image} alt={card.title} />
-        <header class="card-header">
-          <p class="h2">{card.title}</p>
-        </header>
-        <section class="p-4">
-          {card.description}
-        </section>
-        <footer class="card-footer flex justify-center">
-          <a href={card.href} class="btn bg-primary-500">{card.buttonText}</a>
-        </footer>
-      </div>
-      <div class="col-span-1">
-        <img
-          src={card.previewImage}
-          alt=""
-          class="img card-hover flex justify-center rounded-md align-middle" />
-      </div>
-    {/if}
-  </a>
+  <div class="my-4 p-40">
+    <a
+      href={card.href}
+      class="relative grid grid-cols-2 items-center justify-center gap-4 space-y-6">
+      {#if card.section % 2 === 0}
+        <div class="col-span-1">
+          <img
+            src={card.previewImage}
+            alt=""
+            class="img card-hover flex justify-center rounded-md align-middle" />
+        </div>
+        <div class="card card-hover col-span-1">
+          <img class="rounded-t-lg" src={card.image} alt={card.title} />
+          <header class="card-header">
+            <p class="h2">{card.title}</p>
+          </header>
+          <section class="p-4">
+            {card.description}
+          </section>
+          <footer class="card-footer flex justify-center">
+            <a href={card.href} class="btn bg-primary-500">{card.buttonText}</a>
+          </footer>
+        </div>
+      {:else}
+        <div class="card card-hover col-span-1">
+          <img class="rounded-t-lg" src={card.image} alt={card.title} />
+          <header class="card-header">
+            <p class="h2">{card.title}</p>
+          </header>
+          <section class="p-4">
+            {card.description}
+          </section>
+          <footer class="card-footer flex justify-center">
+            <a href={card.href} class="btn bg-primary-500">{card.buttonText}</a>
+          </footer>
+        </div>
+        <div class="col-span-1">
+          <img
+            src={card.previewImage}
+            alt=""
+            class="img card-hover flex justify-center rounded-md align-middle" />
+        </div>
+      {/if}
+    </a>
+  </div>
 </div>
 
 <style>
